67285c31e3596a5c910eb3e0796925a2542065ef
alpha <alpha@a>
2017-07-25 23:25:53 +0900

    Commit!!

--- chamberofsecrets/src/chamberofsecrets.cpp
+++ chamberofsecrets/src/chamberofsecrets.cpp
@@ -5,11 +5,16 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
+using namespace cv;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	VideoCapture vc("C:/upload/sample.mp4");
+	cout << "Start";
+	Mat frame;
+	vc >> frame; // Get First Image
+	imwrite("C:/upload/first_image.jpg", frame);
 	return 0;
 }
